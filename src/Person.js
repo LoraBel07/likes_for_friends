ff3c8530d1794631f5f7998f862fcf43201e908c
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-01-04 16:01:22 -0500

    commit

--- src/Person.js
+++ src/Person.js
@@ -1,6 +1,6 @@
 function Person(props) {
 	return (
-		<div className="one">
+		<div className="box">
 		<img src={props.herimg} alt="photo" width="200px"/>
 		<h2>{props.hername}</h2>
 		<p>{props.herage}</p>
